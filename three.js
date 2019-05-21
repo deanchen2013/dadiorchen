3d16b096a6782da5df93f32ea43d9ffb7e51b9e3
deanchen <antlet2004@gmail.com>
2019-05-21 15:39:47 +0800

    code a HTML DOM element into 3d scene, a div and a png

--- three.js
+++ three.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 if(process.browser){
 	window.THREE = THREE;
 	require('three/examples/js/controls/OrbitControls');
+	require('three/examples/js/renderers/CSS3DRenderer.js')
 }
 
 global.THREE		= THREE
